8dfe14f6a4f70d6447bc0cf9c4dfcb6fdc491264
Farouk <faroukwebdesign@gmail.com>
2024-04-27 16:08:52 +0100

    feat(expertise section): update responsiveness of expertise section
    updated the responsiveness of expertise section , still being tested to meet with the overall design
    of the website
    
    fix #11

--- src/components/Cards/SkillCard.jsx
+++ src/components/Cards/SkillCard.jsx
@@ -3,12 +3,12 @@ import { TbBrandNextjs } from "react-icons/tb"
 
 function SkillCard({ icon: IconComponent, description, title }) {
   return (
-    <div className="h-[17rem] w-full bg-white-200 rounded-3xl p-8">
-      <div className="flex flex-col gap-4 h-full w-full p-2 justify-between">
+    <div className="h-[100%] w-full bg-white-200 rounded-3xl p-8">
+      <div className="flex flex-col h-full w-full gap-7 p-2">
         <span className="text-5xl text-black">{IconComponent}</span>
-        <div className="w-full flex flex-col gap-3">
+        <div className="w-full flex flex-col gap-2 h-full">
           <h2 className="text-2xl text-black font-[500]">{title}</h2>
-          <p className="text-md text-black">{description}</p>
+          <p className="text-md text-black w-full">{description}</p>
         </div>
       </div>
     </div>
--- src/components/SkillsSection/index.jsx
+++ src/components/SkillsSection/index.jsx
@@ -8,7 +8,7 @@ import { SiTailwindcss } from "react-icons/si"
 function SkillSection() {
   return (
     <section className="min-h-fit w-full">
-      <div className="min-h-fit w-full flex flex-col gap-4">
+      <div className="min-h-fit w-full flex flex-col gap-6">
         <div className="min-h-fit w-full flex flex-col gap-2 items-center p-4 lg:my-2 2xl:my-4 my-2">
           <h2 className="lg:text-5xl xl:text-6xl 2xl:text-7xl text-4xl font-[500] text-black">
             Top Skills
@@ -17,34 +17,42 @@ function SkillSection() {
             The best of me to bring your product stand-out
           </p>
         </div>
-        <div className="flex xl:flex-row flex-col gap-5 w-full ">
-          <div className="xl:w-[61%] w-full">
+        <div className="flex xl:flex-row flex-col gap-6 w-full ">
+          <div className="flex-1 w-full">
             <SkillCard
-              title={"Javascript"}
-              description={"A Dynamic scripting for web interactivity."}
+              title={"JavaScript"}
+              description={
+                "An essential web scripting language for interactive, dynamic development in modern web applications."
+              }
               icon={<RiJavascriptFill className="text-yellow-400" />}
             />
           </div>
-          <div className="xl:w-[49%] w-full">
+          <div className="xl:w-[42%] w-full">
             <SkillCard
               title={"NextJS"}
-              description={"A React-based framework for web applications."}
+              description={
+                "A Framework for React.js, simplifying server-side rendering and providing efficient, scalable web applications."
+              }
               icon={<TbBrandNextjs />}
             />
           </div>
         </div>
         <div className="flex xl:flex-row flex-col gap-5 w-full ">
-          <div className="xl:w-[49%] w-full">
+          <div className="xl:w-[42%] w-full">
             <SkillCard
               title={"ReactJS"}
-              description={"A JavaScript library for building interfaces."}
+              description={
+                "A JavaScript library for building user interfaces, facilitating component-based, fast, and interactive web development."
+              }
               icon={<FaReact className="text-blue-400" />}
             />
           </div>
-          <div className="xl:w-[61%] w-full">
+          <div className="flex-1 w-full">
             <SkillCard
               title={"TailwindCSS"}
-              description={"A Utility-first CSS framework for flexibility."}
+              description={
+                "A Utility-first CSS framework for building custom, responsive designs with ease and speed."
+              }
               icon={<SiTailwindcss className="text-cyan-600" />}
             />
           </div>
