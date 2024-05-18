492c6260156e0d70f5ea1e936815541a539e1a43
farouk26 <faroukwebdesign@gmail.com>
2024-05-18 01:57:10 +0100

    fix(about page): update the UI of the about page
    updated the UI more
    
    fix #23

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -13,22 +13,67 @@ function AboutPage() {
     <section className="min-h-screen w-full bg-white-100">
       <Layout>
         <div className="min-h-fit w-full flex flex-col gap-5">
-          {/* Header */}
-          <div className="min-h-fit w-full flex xl:flex-row flex-col gap-4">
+          {/* Header Section */}
+          <div className="h-[33rem] w-full flex xl:flex-row flex-col gap-4">
             {/* First Column */}
-            <div className="xl:w-[60%] w-full bg-white-200 rounded-3xl flex flex-col">
-              <div className="flex flex-col gap-2 w-full min-h-fit">
-                <p className="text-dark-100 font-[400] text-2xl">About me</p>
+            <div className="xl:w-[52%] h-full w-full bg-white-200 rounded-3xl flex flex-col p-9 justify-between">
+              <div className="flex flex-col gap-4 w-full min-h-fit">
+                <p className="text-dark-100 font-[500] text-3xl">About me</p>
                 <h1 className="text-dark-100 text-7xl font-[500]">
-                  Creative Front-End Web Developer
+                  Creative Frontend Web Developer
                 </h1>
               </div>
-              <div className="w-full flex gap-2">
-                <button className="btn btn-lg bg-dark-200 text-white-200 font-[300] uppercase">
+              <div className="w-full flex gap-3 justify-center">
+                <button className="btn btn-lg bg-dark-200 w-[49%] text-white-200 font-[300] uppercase rounded-full">
                   Get In Touch
+                </button>
+                <button className="btn btn-lg btn-ghost border-dark-100 text-dark-100 w-[49%] hover:text-dark-100 hover:border-dark-100 font-[300] uppercase rounded-full">
+                  Download My CV
                 </button>
               </div>
             </div>
+            {/* Second Column */}
+            <div className="h-full w-[48%] flex flex-col gap-4">
+              <div className="h-[60%] w-full bg-white-200 rounded-3xl p-8 flex items-center">
+                <p className="text-dark-100 font-[300] xl:text-lg 3xl:text-xl">
+                  I&apos;m a dedicated Front-End Developer with experience in
+                  collaborative team projects and a strong background in
+                  creating responsive and visually appealing web interfaces.
+                  Skilled in HTML/CSS, JavaScript, Tailwind CSS, React, Next.js
+                  and Firebase. I actively participated in four distinct
+                  projects, collaborating with different teams and showcasing my
+                  versatility as a developer.
+                </p>
+              </div>
+              <div className="h-[40%] w-full bg-white-200 rounded-3xl p-8 flex items-center">
+                <p className="text-dark-100 font-[300] xl:text-lg 3xl:text-xl">
+                  For each project. I strive to create elegant and meaningful
+                  solutions for the end user. I can work independently on
+                  projects as well as collaboratively within a team if reauired.
+                  I ensure seamless execution and optimal results.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Picture Section */}
+          <div className="xl:h-[38.7rem] 3xl:h-full w-full rounded-3xl flex flex-row gap-4">
+            {/* First Column */}
+            <div className="w-[42%] flex flex-col gap-4">
+              <DetailCard />
+              <DetailCard />
+              <DetailCard />
+            </div>
+            {/* Second Column */}
+            <figure className="w-[58%]">
+              <Image
+                alt="personal-pic"
+                src={"/images/farouk.png"}
+                height={800}
+                width={800}
+                priority
+                className="w-full h-full object-cover rounded-3xl"
+              />
+            </figure>
           </div>
         </div>
       </Layout>
