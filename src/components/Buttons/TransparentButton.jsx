dbba2f3d7dc0cff5398e9ce8abc762ea6ee6acdf
farouk26 <faroukwebdesign@gmail.com>
2024-06-23 23:45:14 +0100

    style(buttons): test new button animation with new color thene
    tested a new animation for the button with a new color theme that compliments the portfolio's main
    theme
    
    fix #27

--- src/components/Buttons/TransparentButton.jsx
+++ src/components/Buttons/TransparentButton.jsx
@@ -2,13 +2,11 @@ import React from "react"
 
 function TransparentButton() {
   return (
-    <button class="overflow-hidden relative w-[25%] btn btn-lg rounded-full bg-black text-white border-none font-bold cursor-pointer z-10 group text-center">
-      Contact Me
-      <span class="absolute w-full h-32 -top-8 bg-green-400 rounded-full transform scale-x-0 group-hover:scale-x-110 transition-transform group-hover:duration-700 duration-700"></span>
-      <span class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 z-10 text-center">
-        Today
+    <div className="button bg-midnight-teal btn btn-lg xl:w-[20%] w-[90%] rounded-full overflow-hidden flex items-center justify-center cursor-pointer">
+      <span className="text-white-200 font-[400] text-lg uppercase">
+        Contact Me
       </span>
-    </button>
+    </div>
   )
 }
 
