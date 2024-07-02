3f3fbed0270b47d15b865580b778d07d52348222
farouk26 <faroukwebdesign@gmail.com>
2024-07-02 08:53:35 +0100

    style(responsiveness): fix about page responsiveness issue , to be tested also on coming commits
    this will be tested until its perfected
    
    fix #27

--- src/components/Cards/DetailCard.jsx
+++ src/components/Cards/DetailCard.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function DetailCard({ title, numbers }) {
   return (
-    <div className="w-full h-full bg-white-200 rounded-3xl p-6">
+    <div className="w-full h-[100%] bg-white-200 rounded-3xl p-6">
       <div className="flex flex-col gap-6 p-4 h-full">
         <p className="font-[400] uppercase text-dark-100 font-poppins">
           {title}
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -16,20 +16,56 @@ function AboutPage() {
       <Layout>
         <div className="min-h-full w-full flex flex-col gap-5">
           {/* Header Section */}
-          <div className="min-h-fit w-full flex lg:flex-row flex-col">
-            <div className="w-[60%] h-full">
-              <div className="w-full h-full bg-white-200 rounded-3xl flex flex-col gap-4 p-6">
-                <div className="min-h-fit w-full flex flex-col p-2 gap-4">
+          <div className="xl:h-[100%] min-h-fit w-full flex lg:flex-row 2xl:flex-col 3xl:flex-row flex-col gap-4">
+            {/* First Column of Header */}
+            <div className="xl:w-[55%] 2xl:w-full w-full min-h-full">
+              <div className="w-full min-h-full bg-white-200 rounded-3xl flex flex-col justify-between 2xl:gap-10 gap-12 p-6 xl:p-8">
+                <div className="min-h-fit w-full flex flex-col p-2 lg:gap-4 gap-6">
                   <p className="text-2xl text-dark-100 font-poppins">
                     About Me
                   </p>
-                  <h1 className="text-dark-100 text-6xl font-lora font-[600]">
+                  <h1 className="text-dark-100 lg:text-7xl xl:text-7xl md:text-6xl text-5xl font-lora font-[600]">
                     I&apos;m versatlile front-end developer
                   </h1>
                 </div>
+                <div className="flex gap-4 flex-row items-center md:justify-start justify-center w-full min-h-fit">
+                  <ColorfulButton
+                    mainText={"See My Resume"}
+                    secondText={"Download My CV"}
+                    widthClasses={"lg:w-[49%] xl:w-[49%] md:w-[35%] w-[49%]"}
+                    btnSize={"md:btn-lg btn-md"}
+                  />
+                  <TransparentButton
+                    mainText={"Get In Touch"}
+                    secondText={"Contact Me"}
+                    widthClasses={"lg:w-[49%] xl:w-[49%] md:w-[35%] w-[49%]"}
+                    btnSize={"md:btn-lg btn-md"}
+                  />
+                </div>
               </div>
             </div>
-            <div className="w-[40%] h-full"></div>
+            {/* Second Column of Header */}
+            <div className="xl:w-[45%] 2xl:w-full w-full h-full flex flex-col gap-4">
+              <div className="w-full h-[1005] rounded-3xl bg-white-200 md:p-7 lg:p-6 xl:p-8 3xl:p-5 p-5">
+                <p className="text-dark-100 font-poppins md:text-lg text-sm font-[400] p-2">
+                  I&apos;m a dedicated front-end developer with experience in
+                  collaborative team projects and a strong background in
+                  creating responsive and vosially appealing web interfaces.
+                  Skilled in HTKL/CSS, JavaScript, Tailwind CSS, Reactjs,
+                  Next.js and Firebase. I actively participated in four distinct
+                  projects, collaborating with different teams and showcasing my
+                  versatility as a developer.
+                </p>
+              </div>
+              <div className="w-full h-[100%] rounded-3xl bg-white-200 md:p-7 lg:p-6 xl:p-8 3xl:p-5 p-5">
+                <p className="text-dark-100 font-poppins md:text-lg text-sm font-[400] p-2">
+                  For each Porject, I strive to create elegant and meaningful
+                  solutions for the end user. I can work independently on
+                  projects as well as collaboratively within a team if reauired,
+                  I ensure seamless execution and optimal results.
+                </p>
+              </div>
+            </div>
           </div>
           <span className="w-full min-h-fit">
             <h1 className="font-[500] text-6xl text-dark-100 p-2 text-center my-4 font-lora">
@@ -41,17 +77,17 @@ function AboutPage() {
           <div className="min-h-fit w-full">
             <div className="flex xl:flex-row flex-col-reverse gap-4 w-full h-full">
               {/* First Column */}
-              <div className="flex flex-col h-full xl:w-[35%] w-full gap-4">
+              <div className="flex flex-col min-h-full xl:w-[35%] w-full gap-4">
                 <DetailCard title={"Years Of Experience"} numbers={"1+"} />
                 <DetailCard title={"Projects Completed"} numbers={"4+"} />
                 <DetailCard title={"Skills Learned"} numbers={"12+"} />
               </div>
               {/* Second Column */}
               <div className="xl:w-[65%] w-full min-h-fit flex flex-col gap-4">
-                <div className="flex xl:flex-row flex-col-reverse gap-4 w-full h-full">
+                <div className="flex xl:flex-row flex-col-reverse gap-4 w-full h-[100%]">
                   {/* First Column */}
-                  <div className="h-full xl:w-[50%] w-full flex xl:flex-col flex-col gap-4">
-                    <div className="w-full h-[35%]">
+                  <div className="h-[100%] xl:w-[50%] w-full flex xl:flex-col flex-col gap-4">
+                    <div className="w-full h-fit">
                       <StatusCard
                         IconComponent={<TbBriefcase />}
                         IconComponent2={<GoDotFill />}
@@ -59,15 +95,15 @@ function AboutPage() {
                         status={"Status"}
                       />
                     </div>
-                    <span className="w-full xl:h-full flex flex-col gap-4 p-8 bg-white-200 rounded-3xl">
-                      <p className="text-dark-100 font-[400] text-xl">
+                    <span className="w-full xl:h-[100%] 3xl:min-h-fit flex flex-col gap-4 p-8 bg-white-200 rounded-3xl">
+                      <p className="text-dark-100 font-[400] text-xl 4xl:text-2xl">
                         This is one of my favorite quotes by{" "}
                         <span className="text-midnight-teal font-poppins font-[500]">
                           Confucius
                         </span>
                         .
                       </p>
-                      <q className="text-dark-100 font-[500] text-4xl font-lora">
+                      <q className="text-dark-100 font-[500] text-4xl 4xl:text-5xl font-lora h-full">
                         The man who moves a mountain begins by carrying away
                         small stones.
                       </q>
